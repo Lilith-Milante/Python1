be01e4363932fa9c883ae2cd3cf06560dfeee0cf
kali <hazine505@gmail.com>
2023-10-16 19:31:01 -0400

    first strings

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+sys.path.append("/home/kali/python1/venv3/lib/python3.11/site-packages/lxml")
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
+def get_ip(machine_name):
+    pass
+
+
+class Scanner:
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    scan = Scanner()
+    print('hello')
